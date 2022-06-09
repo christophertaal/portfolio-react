404bcb9596e21a3d3d59a4bf99dc02b39b57e73a
christophertaal <63666767+christophertaal@users.noreply.github.com>
2022-06-10 01:33:44 +0200

    Update About.js

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -14,7 +14,7 @@ const About = () => {
                 Front End Web Developement is an initial step towards a bigger world of Informative Technology. This makes me feel alived and motivated to face another chapter of my career.
                 </div>
                 <div className="container pt-5">
-                I want to combine my clinical skills with my knowledge on frontend web developement. I want to improve the on-going clinical platforms that are used in many clinical settings to gather health data's inorder to analyze pasient care outcomes and train health propffesionals in new pasient care technology.
+                To begin with, I want to combine my clinical skills with my knowledge on frontend web developement. I want to improve the on-going clinical platforms that are used in many clinical settings to gather health data's inorder to analyze pasient care outcomes and train health propffesionals in new pasient care technology.
                 </div>
                 </div>
                 
